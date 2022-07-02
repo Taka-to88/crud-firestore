1b25b090875d2f2c83b8eec7bc1f89daf7488215
TaKATO KONDO <takatokondou@TaKATOnoMacBook-Pro.local>
2022-07-02 10:36:49 +0900

    test

--- main.py
+++ main.py
@@ -1,75 +1,145 @@
+# import json
+# from flask import Flask, g, request   
+# import firebase_admin
+# from firebase_admin import firestore    
+
+# # dbpath = 'test.db' #テーブルを保存するファイル
+
+# def crud_db(request):
+#     app = Flask(__name__)
+
+#     # 初期化済みのアプリが存在しないか確認する。※複数アプリの初期化はエラーです。的な例外に遭遇したので入れたif文
+#     if len(firebase_admin._apps) == 0:
+#         # アプリを初期化する
+#         default_app = firebase_admin.initialize_app()
+#     db = firestore.client()
+        
+#     # class User(db.Model):
+#     #     name = db.Culumn(db.String(20), unique=True)
+#     #     password = db.Culumn(db.String(30))
+#     #     email = db.Culumn(db.String(50), unique=True)
+
+
+#     @app.route('/', methods=['GET'])
+#     def get_user():        
+#         docs = con.collection('users').get()
+#         users_list = []
+#         for doc in docs:
+#             users_list.append(doc.to_dict())
+#         return_json = json.dumps({"users": users_list}, ensure_ascii=False)
+
+#         return return_json
+
+
+#     @app.route('/', methods=['POST'])
+#     def create_data():
+#         # 新しいユーザーの追加
+#         name = request.form["name"] #POSTメソッド のデータを取得
+#         password = request.form["password"]
+#         email = request.form["email"]
+#         user = ({'name': name, 'password': password, 'email':email})
+#         db.collection('users').add({user})
+
+#         # 既存ユーザーの情報追加
+#         # item = ({"name": "太郎","age": "26","sex": "Male"})
+#         # db.collection('users').document('CB1KCDj7CkWGv23IEt4R').set(item)
+        
+#         # ブラウザに見せるために返す
+#         return f'Create User!'
+
+
+#     @app.route('/<id>', methods=['PUT'])
+#     def update_data(id):
+#         #PUTメソッド のデータを取得
+#         name = request.form["name"] #PUTメソッド のデータを取得
+#         password = request.form["password"]
+#         email = request.form["email"]
+#         user_item = ({'name': name, 'password': password, 'email':email})
+#         # 既存ユーザーの情報更新
+#         db.collection('users').document({id}).update({user_items})
+        
+#         # ブラウザに見せるために返す
+#         return f'Update Data!'
+
+
+#     @app.route('/<id>', methods=['DELETE'])
+#     def delete_data(id):
+#         # 既存ユーザーの情報更新
+#         db.collection('users').document({id}).delete()
+        
+#         # ブラウザに見せるために返す
+#         return f'Delete Data!'
+
+
+#     if __name__ == "__main__":
+#         app.run(debug=False, port=80)
+
+
+
 import json
-from flask import Flask, g, request   
+from google.cloud import firestore
+
+
+def get_user(request):
+    db = firestore.Client()
+
+    docs = db.collection('users').get()
+    users_list = []
+    for doc in docs:
+        users_list.append(doc.to_dict())
+    return_json = json.dumps({"users": users_list}, ensure_ascii=False)
+
+    return return_json
+
+
 import firebase_admin
-from firebase_admin import firestore    
+from firebase_admin import firestore
 
-# dbpath = 'test.db' #テーブルを保存するファイル
 
-def crud_db(request):
-    app = Flask(__name__)
 
+def create_data(request):
     # 初期化済みのアプリが存在しないか確認する。※複数アプリの初期化はエラーです。的な例外に遭遇したので入れたif文
     if len(firebase_admin._apps) == 0:
         # アプリを初期化する
         default_app = firebase_admin.initialize_app()
     db = firestore.client()
-        
-    # class User(db.Model):
-    #     name = db.Culumn(db.String(20), unique=True)
-    #     password = db.Culumn(db.String(30))
-    #     email = db.Culumn(db.String(50), unique=True)
+
+    # 新しいユーザーの追加
+    user = ({'name': 'ジョニオ', 'age': '89', 'birthplace':'アメリカ'})
+    db.collection('users').add(user)
+
+    # 既存ユーザーの情報追加
+    item = ({"name": "太郎","age": "26","sex": "Male"})
+    db.collection('users').document('CB1KCDj7CkWGv23IEt4R').set(item)
 
 
-    @app.route('/', methods=['GET'])
-    def get_user():        
-        docs = con.collection('users').get()
-        users_list = []
-        for doc in docs:
-            users_list.append(doc.to_dict())
-        return_json = json.dumps({"users": users_list}, ensure_ascii=False)
-
-        return return_json
+    # ブラウザに見せるために返す
+    return f'Create User!'
 
 
-    @app.route('/', methods=['POST'])
-    def create_data():
-        # 新しいユーザーの追加
-        name = request.form["name"] #POSTメソッド のデータを取得
-        password = request.form["password"]
-        email = request.form["email"]
-        user = ({'name': name, 'password': password, 'email':email})
-        db.collection('users').add({user})
-
-        # 既存ユーザーの情報追加
-        # item = ({"name": "太郎","age": "26","sex": "Male"})
-        # db.collection('users').document('CB1KCDj7CkWGv23IEt4R').set(item)
-        
-        # ブラウザに見せるために返す
-        return f'Create User!'
+def update_data(request):
+    # 初期化済みのアプリが存在しないか確認する。※複数アプリの初期化はエラーです。的な例外に遭遇したので入れたif文
+    if len(firebase_admin._apps) == 0:
+        # アプリを初期化する
+        default_app = firebase_admin.initialize_app()
+    db = firestore.client()
+    
+    # 既存ユーザーの情報更新
+    db.collection('users').document('CB1KCDj7CkWGv23IEt4R').update({'hobby':'fishing'})
+    
+    # ブラウザに見せるために返す
+    return f'Update Data!'
 
 
-    @app.route('/<id>', methods=['PUT'])
-    def update_data(id):
-        #PUTメソッド のデータを取得
-        name = request.form["name"] #PUTメソッド のデータを取得
-        password = request.form["password"]
-        email = request.form["email"]
-        user_item = ({'name': name, 'password': password, 'email':email})
-        # 既存ユーザーの情報更新
-        db.collection('users').document({id}).update({user_items})
-        
-        # ブラウザに見せるために返す
-        return f'Update Data!'
-
-
-    @app.route('/<id>', methods=['DELETE'])
-    def delete_data(id):
-        # 既存ユーザーの情報更新
-        db.collection('users').document({id}).delete()
-        
-        # ブラウザに見せるために返す
-        return f'Delete Data!'
-
-
-    if __name__ == "__main__":
-        app.run(debug=False, port=80)
+def delete_data(request):
+    # 初期化済みのアプリが存在しないか確認する。※複数アプリの初期化はエラーです。的な例外に遭遇したので入れたif文
+    if len(firebase_admin._apps) == 0:
+        # アプリを初期化する
+        default_app = firebase_admin.initialize_app()
+    db = firestore.client()
+    
+    # 既存ユーザーの情報更新
+    db.collection('users').document('CB1KCDj7CkWGv23IEt4R').delete()
+    
+    # ブラウザに見せるために返す
+    return f'Delete Data!'
